d2f7f02bf3bd85205e57a0b8857c3c37c649682a
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-04-22 22:10:43 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="janc",
+    version="0.1",
+    author="Your Name",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'cantera',
+        'jaxamr @ git+https://github.com/luofx23/JAX-AMR.git'
+    ]
+)
