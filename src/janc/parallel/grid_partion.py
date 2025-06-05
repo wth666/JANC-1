f398a6ea49fd880f8f9f12ddc65dd90e34949af9
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-06-05 18:08:51 +0800

    Update grid_partion.py

--- src/janc/parallel/grid_partion.py
+++ src/janc/parallel/grid_partion.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 devices = jax.devices()
 num_devices = len(devices)
 
-def split_and_distribute_grid(grid,split_aix=1):
+def split_and_distribute_grid(grid,split_axis=1):
     nx = grid.shape(split_axis)
     assert nx % num_devices == 0, "nx should be divisible by number of avaliable devices"
     shards = jnp.split(grid,num_devices,axis=1)
