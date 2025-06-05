4f35d7b4ef22745a0abba64087cc630abff04a73
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-06-05 16:06:01 +0800

    Update grid_partion.py

--- src/janc/parallel/grid_partion.py
+++ src/janc/parallel/grid_partion.py
@@ -11,3 +11,25 @@ def split_and_distribute_grid(grid,split_aix=1):
     shards = jnp.split(grid,num_devices,axis=1)
     sharded_grid = jax.device_put_sharded(shards, devices)
     return sharded_grid
+
+def split_and_distribute_blk_info(blk_info):
+    number = blk_info['number']
+    index = blk_info['index']
+    glob_index = blk_info['glob_index']
+    neighbor_index = blk_info['neighbor_index']
+
+    number = [number for i in range(num_devices)]
+    index = [index for i in range(num_devices)]
+    glob_index = [glob_index for i in range(num_devices)]
+    neighbor_index = [neighbor_index for i in range(num_devices)]
+
+    sharded_number = jax.device_put_sharded(number, devices)
+    sharded_index = jax.device_put_sharded(index, devices)
+    sharded_glob_index = jax.device_put_sharded(glob_index, devices)
+    sharded_neighbor_index = jax.device_put_sharded(neighbor_index, devices)
+    
+    sharded_blk_info = {'number': sharded_number,
+                        'index': sharded_index,
+                        'glob_index': sharded_glob_index,
+                        'neighbor_index': sharded_neighbor_index}
+    return sharded_blk_info
