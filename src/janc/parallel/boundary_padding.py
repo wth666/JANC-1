8592098166fdb5d0cb8780d91d88bbccfede0be1
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-06-19 19:53:24 +0800

    Update boundary_padding.py

--- src/janc/parallel/boundary_padding.py
+++ src/janc/parallel/boundary_padding.py
@@ -23,3 +23,32 @@ def pad(U,aux):
     field_periodic_pad = jnp.concatenate([field_periodic_x[:,:,-4:-3],field_periodic_x[:,:,-3:-2],field_periodic_x[:,:,-2:-1],field_periodic_x,field_periodic_x[:,:,1:2],field_periodic_x[:,:,2:3],field_periodic_x[:,:,3:4]],axis=2)
     U_periodic_pad,aux_periodic_pad = field_periodic_pad[0:-2],field_periodic_pad[-2:]
     return U_periodic_pad,aux_periodic_pad
+
+
+@jit
+def get_ghost_block_data(blk_data, blk_info):
+
+    num = 3
+
+    neighbor = blk_info['local_neighbor_index']
+    device = blk_info['device_index']
+    my_device = jax.lax.axis_index('x')
+    num_local_blocks = neighbor.shape(0)
+    
+    perm = [(my_device, device[i]) for i in range(num_local_blocks)]
+    
+
+    
+    upper = blk_data[neighbor[:,0], :, -num:, :]
+    lower = blk_data[neighbor[:,1], :, :num, :]
+    left = blk_data[neighbor[:,2], :, :, -num:]
+    right = blk_data[neighbor[:,3], :, :, :num]
+
+    padded_horizontal = jnp.concatenate([left, blk_data, right], axis=3)
+
+    pad_upper = jnp.pad(upper, ((0,0), (0,0), (0,0), (num,num)), mode='constant', constant_values=jnp.nan) 
+    pad_lower = jnp.pad(lower, ((0,0), (0,0), (0,0), (num,num)), mode='constant', constant_values=jnp.nan)
+
+    ghost_blk_data = jnp.concatenate([pad_upper, padded_horizontal, pad_lower], axis=2)
+
+    return ghost_blk_data
