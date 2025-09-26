68ef9724e5141b4be1b1b81fe864d5e456795689
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-09-26 14:25:38 +0800

    Add files via upload

---
+++ src/janc_v2/solver_2D/finite_difference/CENTRAL6.py
@@ -0,0 +1,47 @@
+import jax.numpy as jnp
+
+def d_dx(f, dx):
+    fm3 = f[:,0:-6,:]
+    fm2 = f[:,1:-5,:]
+    fm1 = f[:,2:-4,:]
+    fp1 = f[:,4:-2,:]
+    fp2 = f[:,5:-1,:]
+    fp3 = f[:,6:,:]
+    
+    center = (-fm3 + 9*fm2
+            -45*fm1 + 45*fp1
+            - 9*fp2 + fp3) / (60*dx)
+    left3 = (f[:,0:1]-8*f[:,1:2]+8*f[:,3:4]-f[:,4:5])/(12*dx)
+    left2 = (f[:,2:3] - f[:,0:1])/(2*dx)
+    left1 = (f[:,1:2] - f[:,0:1])/dx
+    
+    right1 = (f[:,-5:-4]-8*f[:,-4:-3]+8*f[:,-2:-1]-f[:,-1:])/(12*dx)
+    right2 = (f[:,-1:] - f[:,-3:-2])/(2*dx)
+    right3 = (f[:,-1:] - f[:,-2:-1])/dx
+    
+    #padded_center = boundary_padding.exchange_halo(center)    
+    return jnp.concatenate([left1,left2,left3,center,right1,right2,right3],axis=1)#padded_center
+
+
+def d_dy(f, dy):
+    fm3 = f[:,:,0:-6]
+    fm2 = f[:,:,1:-5]
+    fm1 = f[:,:,2:-4]
+    fp1 = f[:,:,4:-2]
+    fp2 = f[:,:,5:-1]
+    fp3 = f[:,:,6:]
+    
+    center = (-fm3 + 9*fm2
+            -45*fm1 + 45*fp1
+            - 9*fp2 + fp3) / (60*dy)
+    
+    left3 = (f[:,:,0:1]-8*f[:,:,1:2]+8*f[:,:,3:4]-f[:,:,4:5])/(12*dy)
+    left2 = (f[:,:,2:3] - f[:,:,0:1])/(2*dy)
+    left1 = (f[:,:,1:2] - f[:,:,0:1])/dy
+    
+    right1 = (f[:,:,-5:-4]-8*f[:,:,-4:-3]+8*f[:,:,-2:-1]-f[:,:,-1:])/(12*dy)
+    right2 = (f[:,:,-1:] - f[:,:,-3:-2])/(2*dy)
+    right3 = (f[:,:,-1:] - f[:,:,-2:-1])/dy
+    
+    return jnp.concatenate([left1,left2,left3,center,right1,right2,right3],axis=2)
+
---
+++ src/janc_v2/solver_2D/finite_difference/__init__.py
@@ -0,0 +1,9 @@
+from . import CENTRAL6
+
+d_dx_dict = {
+    "CENTRAL6": CENTRAL6.d_dx
+}
+
+d_dy_dict = {
+    "CENTRAL6": CENTRAL6.d_dy
+}
