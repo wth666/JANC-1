b3e43a1476e9fc8a7a5b38be1be520ab84dced78
wth666 <121304181+wth666@users.noreply.github.com>
2025-10-11 16:55:12 +0800

    Update __init__.py

--- src/janc_v2/solver_2D/riemann_solver/__init__.py
+++ src/janc_v2/solver_2D/riemann_solver/__init__.py
@@ -1,6 +1,8 @@
-from . import AUSMPW_plus, HLLC
+from . import AUSMPW_plus, HLLC, KNP
 
 riemann_solver_dict = {
     "AUSMPW_plus": AUSMPW_plus.riemann_flux,
-    "HLLC": HLLC.riemann_flux
+    "HLLC": HLLC.riemann_flux,
+    "KNP": KNP.riemann_flux
 }
+
