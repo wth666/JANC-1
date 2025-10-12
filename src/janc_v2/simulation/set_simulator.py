633f273835a0cd57bbd46317f099201dbe168009
wth666 <121304181+wth666@users.noreply.github.com>
2025-10-12 15:00:25 +0800

    Update set_simulator.py

--- src/janc_v2/simulation/set_simulator.py
+++ src/janc_v2/simulation/set_simulator.py
@@ -118,8 +118,8 @@ def set_advance_func(dim,flux_config,reaction_config,time_control,is_amr,flux_fu
                 U, aux = advance_flux(U,aux,dx,dy,dt,theta)
                 #dU = reaction_model.reaction_source_terms(U,aux,dt,theta)
                 #U = U + dU
-                dU = reaction_model.reaction_source_terms(U[:,:,320:],aux[:,:,320:],dt,theta)
-                U = jnp.concatenate([U[:,:,:320],U[:,:,320:]+dU],axis=2)
+                dU = reaction_model.reaction_source_terms(U[:,:,theta['nonChem']:],aux[:,:,theta['nonChem']:],dt,theta)
+                U = jnp.concatenate([U[:,:,:theta['nonChem']],U[:,:,theta['nonChem']:]+dU],axis=2)
                 aux = update_func(U, aux)
                 return U, aux
     else:
@@ -532,6 +532,7 @@ def AMR_Simulator(simulation_config):
         blk_data = jnp.array([jnp.concatenate([U,aux],axis=0)])
         return blk_data
     return jit(advance_func_amr,static_argnames='level'),jit(advance_func_base)
+
 
 
 
